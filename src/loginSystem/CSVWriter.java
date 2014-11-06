5f27530b881505284a18ed3dc9e26e85f0b997a0
kirththiga <kirththiga.murugupillai@mail.mcgill.ca>
2014-11-05 23:08:16 -0500

    Adding CSVreader and CSVwriter

--- src/loginSystem/CSVWriter.java
+++ src/loginSystem/CSVWriter.java
@@ -2,15 +2,19 @@ package loginSystem;
 import java.io.FileWriter;
 import java.io.IOException;
 
-public class CSVWriter {
+import au.com.bytecode.opencsv.CSVReader;
+import au.com.bytecode.opencsv.CSVWriter;
+
+public class CSVwriter {
 	
 	public static void CSVwriter(String User, String Pass) throws IOException{
-		CSVWriter writer = new CSVWriter(new FileWriter("Your file.csv"), ',');
-			
-		Account newUser = new Account(User, Pass);
+		String csv = "testfile.csv";
+		String emp = "emptyfile.csv";
 
+		CSVWriter writer = new CSVWriter(new FileWriter((emp), true), ',');	
+		Account newUser = new Account(User, Pass);
+		writer.writeNext(newUser.toCSVEntry());
 		writer.close();
-		CSVReader.CSVreader();
 	}
 	
 	/*
