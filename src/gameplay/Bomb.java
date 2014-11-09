61b278ddf9862349966bee03f84fe3d5a7336a33
M Fong <matfong52@gmail.com>
2014-11-08 21:44:34 -0500

    Implemented timer on bombs.

--- src/gameplay/Bomb.java
+++ src/gameplay/Bomb.java
@@ -1,17 +1,25 @@
 package gameplay;
 
 import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.ImageIcon;
+import javax.swing.Timer;
 
-public class Bomb extends GameObject {
+public class Bomb extends GameObject implements ActionListener{
 	private boolean isSolid;
 	private int explosionRadius;
+	private Timer bombTimer;
+	private final int countdownTimer=3000;//in miliseconds
+	
 	public Bomb(Rectangle location, World world){
 		super(location); 
 		isSolid=true;
 		this.world=world;
 		sprite=new ImageIcon("./src/gameplay/BombSprite.png").getImage();
+		bombTimer=new Timer(countdownTimer,this);
+		bombTimer.start();
 		
 	}
 	
@@ -20,5 +28,8 @@ public class Bomb extends GameObject {
 		
 	}
 	
+	public void actionPerformed(ActionEvent e){
+		this.explode();
+	}
 	
 }
