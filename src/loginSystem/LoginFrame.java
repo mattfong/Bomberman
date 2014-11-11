8b1f727bf314edab3a57dc182f96365ec078f676
TheMysteriousMooseRider <philiphoddinott@gmail.com>
2014-11-10 19:37:13 -0500

    Login files

---
+++ src/loginSystem/LoginFrame.java
@@ -0,0 +1,31 @@
+package loginSystem;
+import gameplay.GamePanel;
+
+import javax.swing.JFrame;
+
+public class LoginFrame extends JFrame{
+	public LoginFrame(){
+		initUI();
+		
+	}	
+	public JFrame frame(){
+		initUI();
+		return null;
+	}
+	private void initUI(){
+		//add(new GamePanel());
+		setSize(992,448);
+		setTitle("Bomberman");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+		setResizable(false);
+	}
+	
+	public static void main (String[] args){
+		
+		LoginFrame loginFrame=new LoginFrame();
+		loginFrame.setVisible(true);
+		
+		
+	}
+}
---
+++ src/loginSystem/UsernameAndPasswordValid.java
@@ -0,0 +1,90 @@
+package loginSystem;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+public class UsernameAndPasswordValid {
+	private static JFrame controllingFrame;
+	public static boolean valid(String User, String Pass){
+		int passLength=Pass.length();
+		int userLength=User.length();
+		boolean userValid = User.matches("\\w+");
+		boolean passValid = Pass.matches("\\w+");
+		boolean ULcase=false, passUC=false, passLC=false;
+		boolean areThereNumbers=Pass.matches(".*\\d+.*");
+		int i=0;
+			
+		if(User.equals("utest")){
+			//THIS IS FOR DEBUGGING PURPOSES, 
+			userValid=true;
+			passValid=true;
+			passLength=9;
+			userLength=9;
+			ULcase=true;
+			passUC=true;
+			passLC=false;
+			return true;
+		}
+		
+		
+		else if(userLength<6){//checks for appropreate size
+			JOptionPane.showMessageDialog(controllingFrame,
+	                "Username should be longer than 6 charicters, you have only "+userLength);
+			return false;
+		}
+		
+		else if(passLength<8){
+			JOptionPane.showMessageDialog(controllingFrame,
+	                "Password should be longer than 8 charicters, you have only "+passLength);
+			return false;
+		}
+		
+		else if(!(userValid)){
+			JOptionPane.showMessageDialog(controllingFrame,
+	                "Username has non acceptable charicters");
+			return false;
+		}//Checks for nnon-approreate claricters in user
+		
+		
+		for(i=0;i<passLength-1;i++){
+			ULcase=Character.isUpperCase(Pass.charAt(i));
+			if(ULcase){
+				passUC=true;
+			}//end else
+		}
+		if(!passUC){
+			JOptionPane.showMessageDialog(controllingFrame,
+	                "Password needs one uppercase letter");
+			return false;
+		}
+		 for(i=0;i<passLength-1;i++){
+			ULcase=Character.isLowerCase(Pass.charAt(i));
+			if(ULcase){
+				passLC=true;
+			}//end else
+		}
+		if(!passLC){
+			JOptionPane.showMessageDialog(controllingFrame,
+	                "Password needs one lowercase letter");
+			return false;
+		}
+		//gets an upper or lower case
+	
+		else if(!areThereNumbers){
+			JOptionPane.showMessageDialog(controllingFrame,
+	                "Password needs one number");
+			return false;
+		}
+		
+		else if(passValid){
+			JOptionPane.showMessageDialog(controllingFrame,
+	                "Password must have one speical charicetr");
+			return false;
+		}//password needs one speical case
+		//password must have one of each the following:upper case, lower case, number speical
+		
+		else
+			return true;
+		
+	}
+}
