b9db423f224bf60d8f0baff200e1b0dbae7c731e
M Fong <matfong52@gmail.com>
2014-11-12 23:33:02 -0500

    Moved picture resources to /res file and changed image calls to use Class.class.getResource method.

--- src/gameplay/Wall.java
+++ src/gameplay/Wall.java
@@ -1,7 +1,5 @@
 package gameplay;
 
-import java.awt.Graphics;
-import java.awt.Image;
 import java.awt.Rectangle;
 
 import javax.swing.ImageIcon;
@@ -12,7 +10,7 @@ public class Wall extends GameObject {
 	public Wall(Rectangle location){
 		super(location); 
 		isSolid=true;
-		sprite=new ImageIcon("./src/gameplay/HardBlock.png").getImage();
+		sprite=new ImageIcon(Wall.class.getResource("/HardBlock.png")).getImage();
 		
 	}
 
