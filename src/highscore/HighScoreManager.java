14b85f8b681d8443016f6a7be63c84b29a4a0665
yazami <yahya.azami@mail.mcgill.ca>
2014-11-27 13:38:10 -0500

    Creating a main class.

--- src/highscore/HighScoreManager.java
+++ src/highscore/HighScoreManager.java
@@ -1,5 +1,15 @@
 package highscore;
 
+import gameplay.statemanagers.GameState;
+import gameplay.statemanagers.GameStateManager;
+import savingSystem.SavedGameManager;
+
 public class HighScoreManager {
+    
+    
+    //private SavedGameManager saveManager = new SavedGameManager();
+    //private GameState currentGame = GameStateManager.getInstance().getCurrentGameState();
+    
+    
 
 }
---
+++ src/main/MainController.java
@@ -0,0 +1,13 @@
+package main;
+
+import loginSystem.LoginMenuView;
+
+public class MainController {
+
+    public static void main(String[] args) {
+	
+	LoginMenuView.main(null);
+
+    }
+
+}
