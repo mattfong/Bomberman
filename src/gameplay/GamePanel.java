mattfong/Bomberman

commit 39b1e6f1ed29315a29fc830705bab458dba5ed4c

Modified how constructors for GamePanel. Two possible constructors, one with param World and one with param Level. World loads the world passed in and level loads the level input..
Modified World.java so that it's consturctor takes in a world.

src/gameplay/GamePanel.java
@@ -6,7 +6,6 @@ import gameplay.overlays.Camera;
 import gameplay.overlays.CountdownTimer;
 import gameplay.overlays.HUD;
 import gameplay.pauseMenu.PauseMenuView;
-import gameplay.statemanagers.GameState;
 import gameplay.statemanagers.GameStateManager;
 
 import java.awt.Color;
@@ -42,20 +41,25 @@ public class GamePanel extends JPanel implements Runnable {
 
     private Thread game;
     private volatile boolean running = false;
-    private GameStateManager stateManager;
+    private GameStateManager gameStateManager;
 
     // GameObjects
     World world;
     ArrayList<GameActor> actorList;
 
-    public GamePanel() {
+    /**
+     * Open the game at the level specified.
+     * 
+     * @param level
+     *            level that is to be loaded.
+     */
 
-	stateManager = GameStateManager.getInstance();
+    public GamePanel(Level level) {
+
 	setPreferredSize(gameDim);
 	setFocusable(true);
 	requestFocus();
-	loadGameLevel();
-
+	world = new World(31, 13, level);
 
 	addKeyListener(InputListener.getInstance());
 	InputListener.setGamePanel(this);
@@ -63,22 +67,26 @@ public class GamePanel extends JPanel implements Runnable {
 
     }
 
-    private void loadGameLevel() {
-	if (loadingSavedGame()) {
-	} else {
-	    world = new World(31, 13);
-	    camera = new Camera(0, world.getBomberman());
-	    actorList = world.getActorList();
-	}
+    public GamePanel(World world) {
+
+	setPreferredSize(gameDim);
+	setFocusable(true);
+	requestFocus();
+	this.world = world;
+
+	addKeyListener(InputListener.getInstance());
+	InputListener.setGamePanel(this);
+	setLayout(null);
     }
 
-    private boolean loadingSavedGame() {
-	GameState state = stateManager.getCurrentGameState();
-	// if (potentialWorld != null) { // if there is no cached world in gamestate then we are loading a game
-	// return true;
-	// }
-	return false; // we are not loading a game
-    }
+    // private boolean loadingSavedGame() {
+    // GameState state = stateManager.getCurrentGameState();
+    //
+    // if ( != null) {
+    // return true;
+    // }
+    // return false; // we are not loading a game
+    // }
 
     @Override
     public void run() {

src/gameplay/World.java
@@ -36,19 +36,20 @@ public class World implements Serializable {
     GameActor bomberman;
     ArrayList<GameActor> actorList;
 
-    public World(int widthInBlocks, int heightInBlocks) {
+    public World(int widthInBlocks, int heightInBlocks, Level level) {
 
 	gridHeight = heightInBlocks;
 	gridWidth = widthInBlocks;
 
 	// prep the world
 	worldGenerator = new WorldGenerator(this, widthInBlocks, heightInBlocks);
-	worldGenerator.generateLevel(Level.L11);
+	worldGenerator.generateLevel(level);
 	grid = worldGenerator.getGrid();
 	actorList = worldGenerator.getActorList();
 
-	registerBomberman(actorList); // add register bomberman to the world so
-	// that we can interact wit him
+	registerBombermanFromList(actorList); // add register bomberman to the
+					      // world so that we can interact
+					      // with him
 
 	// prep the timer
 	gameTimer = new CountdownTimer();
@@ -68,7 +69,7 @@ public class World implements Serializable {
 	return bomberman;
     }
 
-    private void registerBomberman(ArrayList<GameActor> actorList) {
+    private void registerBombermanFromList(ArrayList<GameActor> actorList) {
 
 	for (GameActor actor : actorList) {
 

commit d4faa896cfb76e13263e623ee33197a7a2b18702

Fixed bug introduced in commit 39b1e6f1ed29315a29fc830705bab458dba5ed4c, took out the loadSavedGame method by mistake during unit testing. Readded it.

src/gameplay/GamePanel.java
@@ -60,7 +60,8 @@ public class GamePanel extends JPanel implements Runnable {
 	setFocusable(true);
 	requestFocus();
 	world = new World(31, 13, level);
-
+	camera = new Camera(0, world.getBomberman());
+	actorList = world.getActorList();
 	addKeyListener(InputListener.getInstance());
 	InputListener.setGamePanel(this);
 	setLayout(null);
@@ -73,20 +74,13 @@ public class GamePanel extends JPanel implements Runnable {
 	setFocusable(true);
 	requestFocus();
 	this.world = world;
+	camera = new Camera(0, world.getBomberman());
+	actorList = world.getActorList();
 
 	addKeyListener(InputListener.getInstance());
 	InputListener.setGamePanel(this);
 	setLayout(null);
     }
-
-    // private boolean loadingSavedGame() {
-    // GameState state = stateManager.getCurrentGameState();
-    //
-    // if ( != null) {
-    // return true;
-    // }
-    // return false; // we are not loading a game
-    // }
 
     @Override
     public void run() {