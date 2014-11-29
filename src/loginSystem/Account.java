05b8a558edd911ed9315a6a554e8d824300facf2
kirththiga <kirththiga.murugupillai@mail.mcgill.ca>
2014-11-28 19:18:33 -0500

    Included total score in Account, CSVreader and CSVwriter

--- src/loginSystem/Account.java
+++ src/loginSystem/Account.java
@@ -6,18 +6,21 @@ public class Account {
 	private String name;
 	private String userName;
 	private String password;
-	
+	private int totalScore;
 	
 	public Account() {
 		this.name = "";
 		this.userName = "";
 		this.password = "";
+		this.totalScore = 0;
 	}
 
-	public Account(String name, String userName, String password) {
+	public Account(String name, String userName, String password, int totalScore) {
+		super();
 		this.name = name;
 		this.userName = userName;
 		this.password = password;
+		this.totalScore = totalScore;
 	}
 
 	public Account(String[] a){
@@ -34,7 +37,7 @@ public class Account {
 	}
 
 	public String[] toCSVEntry(){
-		String[] entry = new String[]{name, userName, password};
+		String[] entry = new String[]{name, userName, password, Integer.toString(totalScore)};
 		return entry;
 	}
 
@@ -60,6 +63,14 @@ public class Account {
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+
+	public int getTotalScore() {
+		return totalScore;
+	}
+
+	public void setTotalScore(int totalScore) {
+		this.totalScore = totalScore;
 	}
 
 }
