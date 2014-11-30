8a2fdb116535b137a074c8c371bad357251f1d45
M Fong <matfong52@gmail.com>
2014-11-29 19:11:32 -0500

    Added way to query levels to determine what level they are.

--- src/gameplay/Level.java
+++ src/gameplay/Level.java
@@ -8,36 +8,138 @@ import gameplay.gameobject.enemies.Oneal;
 import gameplay.gameobject.enemies.Ovapi;
 import gameplay.gameobject.enemies.Pass;
 
-public enum Level {
-    L1(Balloon.class,Balloon.class,Balloon.class,Balloon.class,Balloon.class,Balloon.class),
-    L2(Balloon.class,Balloon.class,Balloon.class,Oneal.class,Oneal.class,Oneal.class),
-    L3(Balloon.class,Balloon.class,Oneal.class,Oneal.class,Doll.class,Doll.class),
-    L4(Balloon.class,Oneal.class,Doll.class,Doll.class,Minvo.class,Minvo.class),
-    L5(Oneal.class,Oneal.class,Oneal.class,Oneal.class,Doll.class,Doll.class,Doll.class),
-    L6(Oneal.class,Oneal.class,Doll.class,Doll.class,Doll.class,Minvo.class,Minvo.class),
-    L7(Oneal.class,Oneal.class,Doll.class,Doll.class,Doll.class,Kondoria.class,Kondoria.class),
-    L8(Oneal.class,Doll.class,Doll.class,Minvo.class,Minvo.class,Minvo.class,Minvo.class),
-    L9(Oneal.class,Doll.class,Minvo.class,Minvo.class,Minvo.class,Minvo.class,Kondoria.class),
-    L10(Oneal.class,Doll.class,Minvo.class,Kondoria.class,Kondoria.class,Kondoria.class,Ovapi.class),
-    L11(Oneal.class,Doll.class,Doll.class,Minvo.class,Minvo.class,Minvo.class,Kondoria.class,Ovapi.class),
-    L12(Doll.class,Doll.class,Doll.class,Minvo.class,Minvo.class,Minvo.class,Kondoria.class,Kondoria.class,Kondoria.class),
-    L13(Ovapi.class,Ovapi.class,Ovapi.class,Ovapi.class,Ovapi.class,Ovapi.class,Ovapi.class,Pass.class),
-    L14(Doll.class,Minvo.class,Minvo.class,Minvo.class,Kondoria.class,Kondoria.class,Kondoria.class,Pass.class),
-    L15(Minvo.class,Minvo.class,Minvo.class,Kondoria.class,Kondoria.class,Kondoria.class,Kondoria.class,Pass.class),
-    L16(Doll.class,Doll.class,Doll.class,Doll.class,Doll.class,Kondoria.class,Kondoria.class,Pass.class),
-    L17(Balloon.class,Balloon.class,Balloon.class,Oneal.class,Oneal.class,Oneal.class,Pass.class,Pass.class),
-    L18(Balloon.class,Oneal.class,Doll.class,Doll.class,Doll.class,Ovapi.class,Pass.class,Pass.class),
-    L19(Oneal.class,Doll.class,Minvo.class,Kondoria.class,Kondoria.class,Ovapi.class,Pass.class,Pass.class),
-    L20(Kondoria.class,Kondoria.class,Kondoria.class,Kondoria.class,Ovapi.class,Ovapi.class,Ovapi.class,Pass.class,Pass.class),
-    L21(Doll.class,Doll.class,Doll.class,Doll.class,Minvo.class,Minvo.class,Minvo.class,Kondoria.class,Pass.class),
-    L22(Doll.class,Doll.class,Minvo.class,Minvo.class,Kondoria.class,Kondoria.class,Ovapi.class,Ovapi.class,Pass.class),
-    L23(Doll.class,Minvo.class,Kondoria.class,Kondoria.class,Kondoria.class,Kondoria.class,Ovapi.class,Ovapi.class,Pass.class),
-    L24(Doll.class,Minvo.class,Kondoria.class,Kondoria.class,Kondoria.class,Kondoria.class,Ovapi.class,Ovapi.class,Pass.class),
-    L25(Oneal.class,Oneal.class,Doll.class,Minvo.class,Kondoria.class,Kondoria.class,Ovapi.class,Ovapi.class,Pass.class);
-    private Class[] classArray;
+import java.util.Map;
 
-    Level(Class... clz) {
+public enum Level {
+    L1(1, Balloon.class, Balloon.class, Balloon.class, Balloon.class, Balloon.class, Balloon.class), L2(2, Balloon.class, Balloon.class, Balloon.class, Oneal.class, Oneal.class, Oneal.class), L3(
+	    3,
+	    Balloon.class,
+	    Balloon.class,
+	    Oneal.class,
+	    Oneal.class,
+	    Doll.class,
+	    Doll.class), L4(4, Balloon.class, Oneal.class, Doll.class, Doll.class, Minvo.class, Minvo.class), L5(
+	    5,
+	    Oneal.class,
+	    Oneal.class,
+	    Oneal.class,
+	    Oneal.class,
+	    Doll.class,
+	    Doll.class,
+	    Doll.class), L6(6, Oneal.class, Oneal.class, Doll.class, Doll.class, Doll.class, Minvo.class, Minvo.class), L7(
+	    7,
+	    Oneal.class,
+	    Oneal.class,
+	    Doll.class,
+	    Doll.class,
+	    Doll.class,
+	    Kondoria.class,
+	    Kondoria.class), L8(8, Oneal.class, Doll.class, Doll.class, Minvo.class, Minvo.class, Minvo.class, Minvo.class), L9(
+	    9,
+	    Oneal.class,
+	    Doll.class,
+	    Minvo.class,
+	    Minvo.class,
+	    Minvo.class,
+	    Minvo.class,
+	    Kondoria.class), L10(10, Oneal.class, Doll.class, Minvo.class, Kondoria.class, Kondoria.class, Kondoria.class, Ovapi.class), L11(
+	    11,
+	    Oneal.class,
+	    Doll.class,
+	    Doll.class,
+	    Minvo.class,
+	    Minvo.class,
+	    Minvo.class,
+	    Kondoria.class,
+	    Ovapi.class), L12(12, Doll.class, Doll.class, Doll.class, Minvo.class, Minvo.class, Minvo.class, Kondoria.class, Kondoria.class, Kondoria.class), L13(
+	    13,
+	    Ovapi.class,
+	    Ovapi.class,
+	    Ovapi.class,
+	    Ovapi.class,
+	    Ovapi.class,
+	    Ovapi.class,
+	    Ovapi.class,
+	    Pass.class), L14(14, Doll.class, Minvo.class, Minvo.class, Minvo.class, Kondoria.class, Kondoria.class, Kondoria.class, Pass.class), L15(
+	    15,
+	    Minvo.class,
+	    Minvo.class,
+	    Minvo.class,
+	    Kondoria.class,
+	    Kondoria.class,
+	    Kondoria.class,
+	    Kondoria.class,
+	    Pass.class), L16(16, Doll.class, Doll.class, Doll.class, Doll.class, Doll.class, Kondoria.class, Kondoria.class, Pass.class), L17(
+	    17,
+	    Balloon.class,
+	    Balloon.class,
+	    Balloon.class,
+	    Oneal.class,
+	    Oneal.class,
+	    Oneal.class,
+	    Pass.class,
+	    Pass.class), L18(18, Balloon.class, Oneal.class, Doll.class, Doll.class, Doll.class, Ovapi.class, Pass.class, Pass.class), L19(
+	    19,
+	    Oneal.class,
+	    Doll.class,
+	    Minvo.class,
+	    Kondoria.class,
+	    Kondoria.class,
+	    Ovapi.class,
+	    Pass.class,
+	    Pass.class), L20(20, Kondoria.class, Kondoria.class, Kondoria.class, Kondoria.class, Ovapi.class, Ovapi.class, Ovapi.class, Pass.class, Pass.class), L21(
+	    21,
+	    Doll.class,
+	    Doll.class,
+	    Doll.class,
+	    Doll.class,
+	    Minvo.class,
+	    Minvo.class,
+	    Minvo.class,
+	    Kondoria.class,
+	    Pass.class), L22(22, Doll.class, Doll.class, Minvo.class, Minvo.class, Kondoria.class, Kondoria.class, Ovapi.class, Ovapi.class, Pass.class), L23(
+	    23,
+	    Doll.class,
+	    Minvo.class,
+	    Kondoria.class,
+	    Kondoria.class,
+	    Kondoria.class,
+	    Kondoria.class,
+	    Ovapi.class,
+	    Ovapi.class,
+	    Pass.class), L24(24, Doll.class, Minvo.class, Kondoria.class, Kondoria.class, Kondoria.class, Kondoria.class, Ovapi.class, Ovapi.class, Pass.class), L25(
+	    25,
+	    Oneal.class,
+	    Oneal.class,
+	    Doll.class,
+	    Minvo.class,
+	    Kondoria.class,
+	    Kondoria.class,
+	    Ovapi.class,
+	    Ovapi.class,
+	    Pass.class);
+
+    private Class[] classArray;
+    private int levelNumber;
+
+    private static Map<Integer, Level> numberToLevelMap;
+
+    public static Level getLevelByNumber(int keyValue) {
+	for (Level level : Level.values()) {
+	    if (level.getLevelNumber() == keyValue) {
+		return level;
+	    }
+	}
+	return Level.L1;
+    }
+
+    public int getLevelNumber() {
+	return levelNumber;
+    }
+
+    Level(int levelKey, Class... clz) {
 	classArray = clz;
+	levelNumber = levelKey;
     }
 
     public Class[] getClassList() {
---
+++ tests/gameplay/LevelTest.java
@@ -0,0 +1,28 @@
+package gameplay;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class LevelTest {
+
+    private Level level;
+
+    @Test
+    public void testGetLevelByNumber() {
+	level = Level.getLevelByNumber(1);
+	assertEquals(Level.L1, level);
+	level = Level.getLevelByNumber(25);
+	assertEquals(Level.L25, level);
+
+    }
+
+    @Test
+    public void testGetLevelNumber() {
+	assertEquals(1, Level.L1.getLevelNumber());
+	assertEquals(14, Level.L14.getLevelNumber());
+	assertEquals(25, Level.L25.getLevelNumber());
+
+    }
+
+}
