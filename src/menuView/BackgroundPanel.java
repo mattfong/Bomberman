b0c394aa7046147f11b9eddaf521ef97fe9c4451
TheMysteriousMooseRider <philiphoddinott@gmail.com>
2014-11-30 13:53:54 -0500

    MenuFrame now works

--- src/menuView/BackgroundPanel.java
+++ src/menuView/BackgroundPanel.java
@@ -1,8 +1,26 @@
 package menuView;
 
+import java.awt.Font;
+import java.awt.GridLayout;
+
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class BackgroundPanel  extends JPanel{
-	
+public class BackgroundPanel extends JPanel {
+	public BackgroundPanel() {
+		initUI();
+	}
 
+	private void initUI() {
+		setLayout(new GridLayout(1, 2, 5, 10));
+		JLabel loginMenuLabel = new JLabel("Login Menu");
+		loginMenuLabel.setFont(new Font("Miriam", Font.BOLD, 28));
+		add(loginMenuLabel);
+
+	}
+	
+	public static void main(String[] args){
+		JPanel panel = new BackgroundPanel();
+		panel.setVisible(true);
+	}
 }
--- src/menuView/MenuDisplay.java
+++ src/menuView/MenuDisplay.java
@@ -2,17 +2,32 @@ package menuView;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
 
 public class MenuDisplay {
 	
-	JFrame MenuFrame = new JFrame("Try1");
+	//JFrame MenuFrame = new JFrame("Try1");
+	JFrame MenuFrame = new JFrame();
 	//JPanel LoginMenuView = new JPanel();
+	JPanel BackgroundPanel = new JPanel();
 	
 	public void display(){
 		//MenuFrame.add(LoadGameMenuView);
+		MenuFrame.add(BackgroundPanel);
+		//MenuFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//MenuFrame.setSize(480, 440);
+		//MenuFrame.setLocationRelativeTo(null);
+		//MenuFrame.setResizable(false);
+		MenuFrame.setVisible(true);
 	}
 	
 	public static void main(String[] args){
-		
+		SwingUtilities.invokeLater(new Runnable(){
+			@Override
+			public void run(){
+				MenuDisplay md = new MenuDisplay();
+				md.display();
+			}
+		});
 	}
 }
