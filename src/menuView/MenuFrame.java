3defc2489bf2ecf93ffbd97a505e3d6d31af20d2
TheMysteriousMooseRider <philiphoddinott@gmail.com>
2014-11-30 13:16:52 -0500

    made menuFrame

---
+++ src/menuView/MenuFrame.java
@@ -0,0 +1,15 @@
+package menuView;
+
+import javax.swing.JFrame;
+
+public class MenuFrame extends JFrame{
+
+	public MenuFrame(){
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(480, 440);
+		setLocationRelativeTo(null);
+		setResizable(false);
+		setVisible(true);
+		
+	}
+}
