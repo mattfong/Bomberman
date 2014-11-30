f4c958cd066fc30d841814d141942823099483c9
yazami <yahya.azami@mail.mcgill.ca>
2014-11-30 13:11:05 -0500

    HighScoreManager: framework for showtopten

--- src/highscore/HighScoreManager.java
+++ src/highscore/HighScoreManager.java
@@ -7,44 +7,52 @@ import gameplay.statemanagers.GameStateManager;
 import savingSystem.SavedGameManager;
 
 public class HighScoreManager {
+
+  
     
-    
-    
-   
-    
-    //TO KIRTH: ADD HERE
-    public static List<String> getPlayerNamesFromCSV(){
-	//this should get data from the savedGame system and input into the String list
-	
-	//setPlayerNames(List<String> list HERE);
-	return null;
-    }
-    
-    //TO KIRTH: ADD HERE
-    public static List<Integer> getPlayerScoresFromCSV(){
-	//this should get data from the savedGame system and input it into the Integer list
-	
-	//setScores(List<Integer> list HERE);
-	return null;
-    }
-    
-    
-    
-    
-    
-    
-    public Players showTopTen(){
-	
-	//get the right info from the getCSVetc. methods above and implement same logic as the main method in HighScoreDatabase
-	//also logic to check if there is anything that is empty or what not should go here. 
-	//when the view creates a HighScoreManager c
-	
-	
-	return null;
+    //Constructor
+    public HighScoreManager(){
 	
     }
     
+ 
+
     
     
+
+    // TO KIRTH: ADD HERE
+    public static List<String> getPlayerNamesFromCSV() {
+	// this should get data from the savedGame system and input into the
+	// String list
+
+	// setPlayerNames(List<String> list HERE);
+	return null;
+    }
+
+    // TO KIRTH: ADD HERE
+    public static List<Integer> getPlayerScoresFromCSV() {
+	// this should get data from the savedGame system and input it into the
+	// Integer list
+
+	// setScores(List<Integer> list HERE);
+	return null;
+    }
+
+    public Players showTopTen() {
+
+	// get the right info from the getCSVetc. methods above and implement
+	// same logic as the main method in HighScoreDatabase
+	// also logic to check if there is anything that is empty or what not
+	// should go here.
+	// when the view creates a HighScoreManager c
+	
+	List<String> names = getPlayerNamesFromCSV();
+	List<Integer> scores = getPlayerScoresFromCSV();
+	
+	
+
+	return null;
+
+    }
 
 }
