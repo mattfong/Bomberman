74d61a901873c9903d499edbc50c40fcb951dee0
M Fong <matfong52@gmail.com>
2014-11-30 00:07:57 -0500

    Added support for loading different powerups based on data stored in level into the game.

--- src/gameplay/Level.java
+++ src/gameplay/Level.java
@@ -7,43 +7,114 @@ import gameplay.gameobject.enemies.Minvo;
 import gameplay.gameobject.enemies.Oneal;
 import gameplay.gameobject.enemies.Ovapi;
 import gameplay.gameobject.enemies.Pass;
-
-import java.util.Map;
+import gameplay.gameobject.powerups.BombPassPowerup;
+import gameplay.gameobject.powerups.BombPowerup;
+import gameplay.gameobject.powerups.DetonatorPowerup;
+import gameplay.gameobject.powerups.FlamePowerup;
+import gameplay.gameobject.powerups.SpeedPowerup;
+import gameplay.gameobject.powerups.WallPassPowerup;
 
 public enum Level {
-    L1(1, Balloon.class, Balloon.class, Balloon.class, Balloon.class, Balloon.class, Balloon.class), L2(2, Balloon.class, Balloon.class, Balloon.class, Oneal.class, Oneal.class, Oneal.class), L3(
+    L1(
+	    1,
+	    FlamePowerup.class,
+	    Balloon.class,
+	    Balloon.class,
+	    Balloon.class,
+	    Balloon.class,
+	    Balloon.class,
+	    Balloon.class),
+    L2(
+	    2,
+
+	    BombPowerup.class,
+	    Balloon.class,
+	    Balloon.class,
+	    Balloon.class,
+	    Oneal.class,
+	    Oneal.class,
+	    Oneal.class),
+    L3(
 	    3,
+	    DetonatorPowerup.class,
 	    Balloon.class,
 	    Balloon.class,
 	    Oneal.class,
 	    Oneal.class,
 	    Doll.class,
-	    Doll.class), L4(4, Balloon.class, Oneal.class, Doll.class, Doll.class, Minvo.class, Minvo.class), L5(
+	    Doll.class),
+    L4(
+	    4,
+	    SpeedPowerup.class,
+	    Balloon.class,
+	    Oneal.class,
+	    Doll.class,
+	    Doll.class,
+	    Minvo.class,
+	    Minvo.class),
+    L5(
 	    5,
+	    BombPowerup.class,
 	    Oneal.class,
 	    Oneal.class,
 	    Oneal.class,
 	    Oneal.class,
 	    Doll.class,
 	    Doll.class,
-	    Doll.class), L6(6, Oneal.class, Oneal.class, Doll.class, Doll.class, Doll.class, Minvo.class, Minvo.class), L7(
+	    Doll.class),
+    L6(
+	    6,
+	    BombPowerup.class,
+	    Oneal.class,
+	    Oneal.class,
+	    Doll.class,
+	    Doll.class,
+	    Doll.class,
+	    Minvo.class,
+	    Minvo.class),
+    L7(
 	    7,
+	    FlamePowerup.class,
 	    Oneal.class,
 	    Oneal.class,
 	    Doll.class,
 	    Doll.class,
 	    Doll.class,
 	    Kondoria.class,
-	    Kondoria.class), L8(8, Oneal.class, Doll.class, Doll.class, Minvo.class, Minvo.class, Minvo.class, Minvo.class), L9(
+	    Kondoria.class),
+    L8(
+	    8,
+	    DetonatorPowerup.class,
+	    Oneal.class,
+	    Doll.class,
+	    Doll.class,
+	    Minvo.class,
+	    Minvo.class,
+	    Minvo.class,
+	    Minvo.class),
+    L9(
 	    9,
+	    BombPassPowerup.class,
 	    Oneal.class,
 	    Doll.class,
 	    Minvo.class,
 	    Minvo.class,
 	    Minvo.class,
 	    Minvo.class,
-	    Kondoria.class), L10(10, Oneal.class, Doll.class, Minvo.class, Kondoria.class, Kondoria.class, Kondoria.class, Ovapi.class), L11(
+	    Kondoria.class),
+    L10(
+	    10,
+	    WallPassPowerup.class,
+	    Oneal.class,
+	    Doll.class,
+	    Minvo.class,
+	    Kondoria.class,
+	    Kondoria.class,
+	    Kondoria.class,
+	    Ovapi.class),
+    L11(
 	    11,
+	    BombPowerup.class,
 	    Oneal.class,
 	    Doll.class,
 	    Doll.class,
@@ -51,8 +122,22 @@ public enum Level {
 	    Minvo.class,
 	    Minvo.class,
 	    Kondoria.class,
-	    Ovapi.class), L12(12, Doll.class, Doll.class, Doll.class, Minvo.class, Minvo.class, Minvo.class, Kondoria.class, Kondoria.class, Kondoria.class), L13(
+	    Ovapi.class),
+    L12(
+	    12,
+	    BombPowerup.class,
+	    Doll.class,
+	    Doll.class,
+	    Doll.class,
+	    Minvo.class,
+	    Minvo.class,
+	    Minvo.class,
+	    Kondoria.class,
+	    Kondoria.class,
+	    Kondoria.class),
+    L13(
 	    13,
+	    DetonatorPowerup.class,
 	    Ovapi.class,
 	    Ovapi.class,
 	    Ovapi.class,
@@ -60,8 +145,21 @@ public enum Level {
 	    Ovapi.class,
 	    Ovapi.class,
 	    Ovapi.class,
-	    Pass.class), L14(14, Doll.class, Minvo.class, Minvo.class, Minvo.class, Kondoria.class, Kondoria.class, Kondoria.class, Pass.class), L15(
+	    Pass.class),
+    L14(
+	    14,
+	    WallPassPowerup.class,
+	    Doll.class,
+	    Minvo.class,
+	    Minvo.class,
+	    Minvo.class,
+	    Kondoria.class,
+	    Kondoria.class,
+	    Kondoria.class,
+	    Pass.class),
+    L15(
 	    15,
+	    FlamePowerup.class,
 	    Minvo.class,
 	    Minvo.class,
 	    Minvo.class,
@@ -69,8 +167,22 @@ public enum Level {
 	    Kondoria.class,
 	    Kondoria.class,
 	    Kondoria.class,
-	    Pass.class), L16(16, Doll.class, Doll.class, Doll.class, Doll.class, Doll.class, Kondoria.class, Kondoria.class, Pass.class), L17(
+	    Pass.class),
+    L16(
+	    16,
+	    WallPassPowerup.class,
+
+	    Doll.class,
+	    Doll.class,
+	    Doll.class,
+	    Doll.class,
+	    Doll.class,
+	    Kondoria.class,
+	    Kondoria.class,
+	    Pass.class),
+    L17(
 	    17,
+	    BombPowerup.class,
 	    Balloon.class,
 	    Balloon.class,
 	    Balloon.class,
@@ -78,8 +190,21 @@ public enum Level {
 	    Oneal.class,
 	    Oneal.class,
 	    Pass.class,
-	    Pass.class), L18(18, Balloon.class, Oneal.class, Doll.class, Doll.class, Doll.class, Ovapi.class, Pass.class, Pass.class), L19(
+	    Pass.class),
+    L18(
+	    18,
+	    BombPassPowerup.class,
+	    Balloon.class,
+	    Oneal.class,
+	    Doll.class,
+	    Doll.class,
+	    Doll.class,
+	    Ovapi.class,
+	    Pass.class,
+	    Pass.class),
+    L19(
 	    19,
+	    BombPowerup.class,
 	    Oneal.class,
 	    Doll.class,
 	    Minvo.class,
@@ -87,8 +212,22 @@ public enum Level {
 	    Kondoria.class,
 	    Ovapi.class,
 	    Pass.class,
-	    Pass.class), L20(20, Kondoria.class, Kondoria.class, Kondoria.class, Kondoria.class, Ovapi.class, Ovapi.class, Ovapi.class, Pass.class, Pass.class), L21(
+	    Pass.class),
+    L20(
+	    20,
+	    DetonatorPowerup.class,
+	    Kondoria.class,
+	    Kondoria.class,
+	    Kondoria.class,
+	    Kondoria.class,
+	    Ovapi.class,
+	    Ovapi.class,
+	    Ovapi.class,
+	    Pass.class,
+	    Pass.class),
+    L21(
 	    21,
+	    WallPassPowerup.class,
 	    Doll.class,
 	    Doll.class,
 	    Doll.class,
@@ -97,8 +236,22 @@ public enum Level {
 	    Minvo.class,
 	    Minvo.class,
 	    Kondoria.class,
-	    Pass.class), L22(22, Doll.class, Doll.class, Minvo.class, Minvo.class, Kondoria.class, Kondoria.class, Ovapi.class, Ovapi.class, Pass.class), L23(
+	    Pass.class),
+    L22(
+	    22,
+	    DetonatorPowerup.class,
+	    Doll.class,
+	    Doll.class,
+	    Minvo.class,
+	    Minvo.class,
+	    Kondoria.class,
+	    Kondoria.class,
+	    Ovapi.class,
+	    Ovapi.class,
+	    Pass.class),
+    L23(
 	    23,
+	    BombPowerup.class,
 	    Doll.class,
 	    Minvo.class,
 	    Kondoria.class,
@@ -107,8 +260,22 @@ public enum Level {
 	    Kondoria.class,
 	    Ovapi.class,
 	    Ovapi.class,
-	    Pass.class), L24(24, Doll.class, Minvo.class, Kondoria.class, Kondoria.class, Kondoria.class, Kondoria.class, Ovapi.class, Ovapi.class, Pass.class), L25(
+	    Pass.class),
+    L24(
+	    24,
+	    DetonatorPowerup.class,
+	    Doll.class,
+	    Minvo.class,
+	    Kondoria.class,
+	    Kondoria.class,
+	    Kondoria.class,
+	    Kondoria.class,
+	    Ovapi.class,
+	    Ovapi.class,
+	    Pass.class),
+    L25(
 	    25,
+	    WallPassPowerup.class,
 	    Oneal.class,
 	    Oneal.class,
 	    Doll.class,
@@ -119,10 +286,15 @@ public enum Level {
 	    Ovapi.class,
 	    Pass.class);
 
-    private Class[] classArray;
+    private Class[] clazzArray;
     private int levelNumber;
+    private Class powerup;
 
-    private static Map<Integer, Level> numberToLevelMap;
+    Level(int levelKey, Class powerup, Class... clz) {
+	this.powerup = powerup;
+	clazzArray = clz;
+	levelNumber = levelKey;
+    }
 
     public static Level getLevelByNumber(int keyValue) {
 	for (Level level : Level.values()) {
@@ -137,13 +309,12 @@ public enum Level {
 	return levelNumber;
     }
 
-    Level(int levelKey, Class... clz) {
-	classArray = clz;
-	levelNumber = levelKey;
+    public Class[] getClassList() {
+	return clazzArray;
     }
 
-    public Class[] getClassList() {
-	return classArray;
+    public Class getPowerupClass() {
+	return powerup;
     }
 
 }
