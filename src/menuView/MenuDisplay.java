9dbe4a0b8dd35b9aadba7b8cb5786365290ebc58
TheMysteriousMooseRider <philiphoddinott@gmail.com>
2014-11-30 13:59:43 -0500

    MenuDisplay now works with frames and panels

--- src/menuView/MenuDisplay.java
+++ src/menuView/MenuDisplay.java
@@ -5,7 +5,7 @@ import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
 public class MenuDisplay {
-	
+	/*
 	//JFrame MenuFrame = new JFrame("Try1");
 	JFrame MenuFrame = new JFrame();
 	//JPanel LoginMenuView = new JPanel();
@@ -19,6 +19,33 @@ public class MenuDisplay {
 		//MenuFrame.setLocationRelativeTo(null);
 		//MenuFrame.setResizable(false);
 		MenuFrame.setVisible(true);
+	}
+	
+	public static void main(String[] args){
+		SwingUtilities.invokeLater(new Runnable(){
+			@Override
+			public void run(){
+				MenuDisplay md = new MenuDisplay();
+				md.display();
+			}
+		});
+	}
+	*/
+	
+	public void display(){
+		//JFrame frame = new JFrame("this is a frame");
+		MenuFrame frame = new MenuFrame("This is my menu fra,e");
+		//JPanel BackgroundPanel = new JPanel();
+		BackgroundPanel panel = new BackgroundPanel();
+		frame.add(panel);
+		
+		//frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			//	frame.setSize(480, 440);
+				//frame.setLocationRelativeTo(null);
+				//frame.setResizable(false);
+				//frame.setVisible(true);
+		
+		
 	}
 	
 	public static void main(String[] args){
