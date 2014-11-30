e3f3233fa4de9214a3437c15d47ee45064f2e44f
TheMysteriousMooseRider <philiphoddinott@gmail.com>
2014-11-30 14:25:52 -0500

    Added delete account Panel

--- src/menuView/MenuDisplay.java
+++ src/menuView/MenuDisplay.java
@@ -5,46 +5,48 @@ import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
 public class MenuDisplay {
-	/*
-	//JFrame MenuFrame = new JFrame("Try1");
-	JFrame MenuFrame = new JFrame();
-	//JPanel LoginMenuView = new JPanel();
-	JPanel BackgroundPanel = new JPanel();
 	
-	public void display(){
-		//MenuFrame.add(LoadGameMenuView);
-		MenuFrame.add(BackgroundPanel);
-		//MenuFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		//MenuFrame.setSize(480, 440);
-		//MenuFrame.setLocationRelativeTo(null);
-		//MenuFrame.setResizable(false);
-		MenuFrame.setVisible(true);
+	public String LM = "loginMenu";
+	public String DM = "deleteMenu";
+	public String NM = "newMenu";
+	DeleteAccountPanel dPanel = new DeleteAccountPanel();
+	LoginMenuView lPanel = new LoginMenuView();
+
+	
+	MenuFrame frame = new MenuFrame("Bomberman");
+	
+	public void loginMenu(){
+		
+		frame.add(lPanel);
 	}
 	
-	public static void main(String[] args){
-		SwingUtilities.invokeLater(new Runnable(){
-			@Override
-			public void run(){
-				MenuDisplay md = new MenuDisplay();
-				md.display();
-			}
-		});
+	public void DeleteAccountPanel(){
+		
+		frame.add(dPanel);
 	}
-	*/
+	
 	
 	public void display(){
 		//JFrame frame = new JFrame("this is a frame");
-		MenuFrame frame = new MenuFrame("This is my menu fra,e");
+		
 		//JPanel BackgroundPanel = new JPanel();
 		BackgroundPanel panel = new BackgroundPanel();
 		frame.add(panel);
 		
-		//frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			//	frame.setSize(480, 440);
-				//frame.setLocationRelativeTo(null);
-				//frame.setResizable(false);
-				//frame.setVisible(true);
 		
+	}
+	
+	public void goTo(String goTo){
+		//frame.removeAll();
+		frame.revalidate();
+		frame.repaint();
+		if(goTo.equals(LM)){
+			loginMenu();
+		}
+		
+		else if(goTo.equals(DM)){
+			DeleteAccountPanel();
+		}
 		
 	}
 	
@@ -53,7 +55,9 @@ public class MenuDisplay {
 			@Override
 			public void run(){
 				MenuDisplay md = new MenuDisplay();
-				md.display();
+				//md.display();
+				//md.loginMenu();
+				md.DeleteAccountPanel();
 			}
 		});
 	}
