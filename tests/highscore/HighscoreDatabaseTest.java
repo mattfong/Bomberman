59af5036d5c431181019fea7bf56f213f5518e6c
kirththiga <kirththiga.murugupillai@mail.mcgill.ca>
2014-12-01 18:29:11 -0500

    Framework for highscore database test

---
+++ tests/highscore/HighscoreDatabaseTest.java
@@ -0,0 +1,19 @@
+package highscore;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class HighscoreDatabaseTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
