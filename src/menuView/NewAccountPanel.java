ad12b2bb141c99561b48c9fff723e29f4d7e457a
TheMysteriousMooseRider <philiphoddinott@gmail.com>
2014-12-01 06:36:54 -0500

    Made new AccountPanel

--- src/menuView/NewAccountPanel.java
+++ src/menuView/NewAccountPanel.java
@@ -1,6 +1,11 @@
 package menuView;
 
+import java.awt.GridLayout;
+
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
 
 public class NewAccountPanel extends JPanel{
 
@@ -11,6 +16,18 @@ public class NewAccountPanel extends JPanel{
 	}
 
 	private void initUI() {
+		setLayout(new GridLayout(7,1,5,10));
+		JTextField UserTF = new JTextField(30);
+		JPasswordField PassPF = new JPasswordField(30);
+		JPasswordField ConfirmPassPF = new JPasswordField(30);
+		UserTF.setText(null);
+		PassPF.setText(null);
+		ConfirmPassPF = new JPasswordField(30);
+		
+		JLabel loginInfo = new JLabel(
+				"Enter the login account you want to delete");
+		JLabel enterUser = new JLabel("Username");
+		JLabel enterPass = new JLabel("Password");
 		
 	}
 }
