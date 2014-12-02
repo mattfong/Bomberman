dcbc3fdb69fbeb73015b36ffdad53c2a734e37ca
TheMysteriousMooseRider <philiphoddinott@gmail.com>
2014-12-01 23:25:47 -0500

    LogicPanelTest

--- tests/loginSystem/LoginMenuPanelLogicTest.java
+++ tests/loginSystem/LoginMenuPanelLogicTest.java
@@ -6,6 +6,7 @@ import java.awt.AWTException;
 import java.awt.HeadlessException;
 import java.awt.Robot;
 import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
 import java.io.IOException;
 import java.util.Random;
 
@@ -57,17 +58,15 @@ public class LoginMenuPanelLogicTest {
 
 	@Test
 	public void testValid() throws HeadlessException, IOException, AWTException {
-		int k,i = 0, uLength, pLength, max = 20, min = 0;
+		int k, i = 0, uLength, pLength, max = 20, min = 0;
 		boolean userValid = false;
 		boolean passValid = false;
 		boolean ULcase = false, passUC = false, userUC = false;
 		boolean passLC = false;
 		boolean areThereNumbers;
-		
+
 		Robot bot = new Robot();
-		
-		
-		
+
 		for (k = 0; k < 1000; k++) {
 			uLength = rnd.nextInt((max - min) + 1) + min;
 			pLength = rnd.nextInt((max - min) + 1) + min;
@@ -80,21 +79,34 @@ public class LoginMenuPanelLogicTest {
 			areThereNumbers = pass.matches(".*\\d+.*");
 
 			// ad in account manager test
-			//add in username with no non standard charircwrs.
-			bot.mouseMove(675,400);
+			// add in username with no non standard charircwrs.
+			bot.mouseMove(675, 400);
 			bot.mousePress(InputEvent.BUTTON1_MASK);
 			bot.mouseRelease(InputEvent.BUTTON1_MASK);
-			//add time between press and release or the input event system may 
-			//not think it is a click
-			try{Thread.sleep(250);}catch(InterruptedException e){}
+			// add time between press and release or the input event system may
+			// not think it is a click
+			try {
+				Thread.sleep(250);
+			} catch (InterruptedException e) {
+			}
 			bot.mouseRelease(InputEvent.BUTTON1_MASK);
 
 			if (uLength < 6) {
 				assertFalse("this should be false", system.valid(user, pass));
+				//bot.keyPress(KEY_ENTER);
+				bot.mousePress(InputEvent.BUTTON1_MASK);
+				bot.mouseRelease(InputEvent.BUTTON1_MASK);
+				bot.keyPress(KeyEvent.VK_ENTER);
+				bot.keyRelease(KeyEvent.VK_ENTER);
 			}
 
 			if (pLength < 8) {
 				assertFalse("this should be false", system.valid(user, pass));
+				bot.mousePress(InputEvent.BUTTON1_MASK);
+				bot.mouseRelease(InputEvent.BUTTON1_MASK);
+				bot.keyPress(KeyEvent.VK_ENTER);
+				bot.keyRelease(KeyEvent.VK_ENTER);
+		
 			}
 
 			// user valid
@@ -104,9 +116,15 @@ public class LoginMenuPanelLogicTest {
 				if (ULcase) {
 					passUC = true;
 				}// end else
+				
 			}
 			if (!passUC) {
 				assertFalse("this should be false", system.valid(user, pass));
+				bot.mousePress(InputEvent.BUTTON1_MASK);
+				bot.mouseRelease(InputEvent.BUTTON1_MASK);
+				bot.keyPress(KeyEvent.VK_ENTER);
+				bot.keyRelease(KeyEvent.VK_ENTER);
+		
 			}
 			for (i = 0; i < pLength - 1; i++) {
 				ULcase = Character.isLowerCase(pass.charAt(i));
@@ -116,28 +134,49 @@ public class LoginMenuPanelLogicTest {
 			}
 			if (!passLC) {
 				assertFalse("this should be false", system.valid(user, pass));
+				bot.mousePress(InputEvent.BUTTON1_MASK);
+				bot.mouseRelease(InputEvent.BUTTON1_MASK);
+				bot.keyPress(KeyEvent.VK_ENTER);
+				bot.keyRelease(KeyEvent.VK_ENTER);
+		
 			}
 			// gets an upper or lower case
 
 			else if (!areThereNumbers) {
 				assertFalse("this should be false", system.valid(user, pass));
+				bot.mousePress(InputEvent.BUTTON1_MASK);
+				bot.mouseRelease(InputEvent.BUTTON1_MASK);
+				bot.keyPress(KeyEvent.VK_ENTER);
+				bot.keyRelease(KeyEvent.VK_ENTER);
+		
 			}
 
 			else if (passValid) {
 				assertFalse("this should be false", system.valid(user, pass));
+				bot.mousePress(InputEvent.BUTTON1_MASK);
+				bot.mouseRelease(InputEvent.BUTTON1_MASK);
+				bot.keyPress(KeyEvent.VK_ENTER);
+				bot.keyRelease(KeyEvent.VK_ENTER);
+		
 			}// password needs one speical case
 				// password must have one of each the following:upper case,
 				// lower case, number speical
 			bot.mousePress(InputEvent.BUTTON1_MASK);
 			bot.mouseRelease(InputEvent.BUTTON1_MASK);
+			bot.keyPress(KeyEvent.VK_ENTER);
+			bot.keyRelease(KeyEvent.VK_ENTER);
+	
+			
+			
+			
 		}
 
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
 	}
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
 	}
 
 }
